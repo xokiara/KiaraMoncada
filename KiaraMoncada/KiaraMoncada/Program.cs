e5a5b22f43ff207dce04535aff5d3365a69373b3
xokiara <kiaramoncada28@gmail.com>
2023-02-23 22:40:07 -0600

    Menu
    Menu

--- KiaraMoncada/KiaraMoncada/Program.cs
+++ KiaraMoncada/KiaraMoncada/Program.cs
@@ -13,7 +13,7 @@ namespace KiaraMoncada
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PrimerFormulario());
+            Application.Run(new Menu());
         }
     }
 }
